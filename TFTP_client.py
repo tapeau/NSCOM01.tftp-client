494bd270a08bac7ae770cfddd0dc8500444f13d7
tapeau <john_lorenzo_tapia@dlsu.edu.ph>
2024-02-12 20:14:16 +0800

    Add welcome screen, address validation, and send_dat()

--- TFTP_client.py
+++ TFTP_client.py
@@ -9,9 +9,11 @@ Credits / References:
 """
 # Import necessary libraries
 import socket # for socket functionalities
+import re # for IP address validation through regex
+from art import * # for design purposes
 
 # Declare constants
-BLKSIZE = 512 # Default is 512
+BLK_SIZE = 512 # Default is 512
 MAX_DATA_LENGTH = BLKSIZE + 4 # BLKSIZE + Opcode + Block Number
 MODE = b'octet' # Only support 'octet' transfer mode since the project only deals with binary files
 
@@ -31,41 +33,101 @@ SERVER_ADDR = None
 
 # TODO: Main function
 def main():
-    pass
+    # Welcome screen
+    print()
+    print(text2art("EASY", space=4))
+    print(text2art("TFTP",font='block'))
+    print("--------------------------------------------------------")
+    print("A simple command-line TFTP client application")
+    print("Developed by Tapia and Madrinan")
+    print("As Machine Project #1 for DLSU NSCOM01 course (T2 2023-2024)")
+    print("--------------------------------------------------------")
+    print()
+
+    # Loop to prompt user to connect to a TFTP server with a valid IP address and port number
+    print("Please connect to a TFTP server to start.")
+    while True:
+        SERVER_ADDR = input("Enter server address: ")
+        if re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+$', SERVER_ADDR):
+            break
+        else:
+            print("ERROR: Please enter a valid IP address with a port number.")
+            SERVER_ADDR = None
+            continue
+    
+    # TODO: Add functions for bonus features (BLKSIZE setting & TSIZE sending)
+    
+    # Main program loop
+    program_loop = True
+    while program_loop:
+        # TODO
+        pass
+
 
 # TODO: Add functions for each TFTP opcode functionality
-# send_rrq()
-# send_wrq()
-# send_dat()
+# send_req() X
+# send_dat() X
+# recv_dat()
 # send_ack()
 # send_err()
 
-def send_rrq(filename):
-    # RRQ packet will be represented as a byte array
-    rrq = bytearray() 
+def send_req(type, filename):
+    '''
+    Function to send TFTP request packet (RRQ or WRQ)
 
-    # Append RRQ opcode at the beginning of the packet
-    rrq.extend(0, OPCODE['RRQ'])
+    Parameters:
+    type (int): Type of request - 1 for RRQ, 2 for WRQ (according to their opcodes)
+    filename (str): Name of file to be included in the request
 
-    # Convert the passed file name into a byte array and append it to the RRQ packet
-    rrq += bytearray(filename.encode('utf-8'))
+    Returns:
+    None
+    '''
+    # Request packet will be represented as a byte array
+    req = bytearray() 
 
-    # Append 0x00 byte between file name and transfer mode
-    rrq.append(0)
+    # Append request opcode at the beginning of the packet
+    req.extend(0, type)
+
+    # Convert the passed file name into a byte array and append it to the request packet
+    req += bytearray(filename.encode('utf-8'))
+
+    # Append 0x00 byte
+    req.append(0)
     
-    # Append transfer mode to RRQ packet
-    rrq += bytearray(bytes(MODE, 'utf-8'))
+    # Append transfer mode to request packet
+    req += bytearray(bytes(MODE, 'utf-8'))
 
-    # Append 0x00 terminal byte
-    rrq.append(0)
+    # Append 0x00 byte
+    req.append(0)
 
-    # Send the packet to server through client socket
-    CLIENT_SOCKET.sendto(rrq, SERVER_ADDR)
+    # Send the packet to the server through client socket
+    CLIENT_SOCKET.sendto(req, SERVER_ADDR)
 
-    # Notify client
-    print(f"File \"{filename}\" requested to server {SERVER_ADDR}")
+def send_dat(block, data):
+    '''
+    Function to send TFTP data packet
 
-# TODO: Add functions for bonus features (BLKSIZE setting & TSIZE sending)
+    Parameters:
+    block (int): Block number of data packet
+    data (bytearray): Bytearray containing data to be sent
+
+    Returns:
+    None
+    '''
+    # Data packet will be represented as a byte array
+    dat = bytearray()
+    
+    # Append data opcode at the beginning of the packet
+    dat.extend(0, OPCODE['DAT'])
+    
+    # Append the block number
+    dat.extend(block.to_bytes(2, byteorder='big', signed=False))
+    
+    # Append the data
+    dat.extend(data)
+    
+    # Send the packet to the server through client socket
+    CLIENT_SOCKET.sendto(dat, SERVER_ADDR)
 
 if __name__ == '__main__':
     main()
